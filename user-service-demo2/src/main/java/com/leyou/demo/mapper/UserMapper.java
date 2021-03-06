85013ab958e36972c84fbd690f0d6b871cc9cfe9
andychan516 <623247195@qq.com>
2021-03-06 19:42:28 +0800

    *删除

--- user-service-demo2/src/main/java/com/leyou/demo/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.leyou.demo.mapper;
-
-import com.leyou.demo.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends tk.mybatis.mapper.common.Mapper<User>{
-}
-
